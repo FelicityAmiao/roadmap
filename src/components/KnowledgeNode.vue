5f5c33afe1d8508afa72ddcb9cd0a5c82185a2bb
felicityamiao <1760077814@qq.com>
2025-05-05 16:55:54 +0800

    using GraphChart

--- src/components/KnowledgeNode.vue
+++ src/components/KnowledgeNode.vue
@@ -6,25 +6,103 @@
 import VChart from 'vue-echarts'
 import { ref } from 'vue'
 
+//paste to here
 import { use } from 'echarts/core'
-import { LineChart } from 'echarts/charts'
-import { GridComponent } from 'echarts/components'
+import { GraphChart } from 'echarts/charts'
+import { TitleComponent, TooltipComponent } from 'echarts/components'
 import { SVGRenderer } from 'echarts/renderers'
 
-use([GridComponent, LineChart, SVGRenderer])
+use([TitleComponent, TooltipComponent, GraphChart, SVGRenderer])
 
 const option = ref({
-  xAxis: {
-    type: 'category',
-    data: ['A', 'B', 'C'],
+  title: {
+    text: 'Basic Graph',
   },
-  yAxis: {
-    type: 'value',
-  },
+  tooltip: {},
+  animationDurationUpdate: 1500,
+  animationEasingUpdate: 'quinticInOut',
   series: [
     {
-      data: [120, 200, 150],
-      type: 'line',
+      type: 'graph',
+      layout: 'none',
+      symbolSize: 50,
+      roam: true,
+      label: {
+        show: true,
+      },
+      edgeSymbol: ['circle', 'arrow'],
+      edgeSymbolSize: [4, 10],
+      edgeLabel: {
+        fontSize: 20,
+      },
+      data: [
+        {
+          name: 'Node 1',
+          x: 300,
+          y: 300,
+        },
+        {
+          name: 'Node 2',
+          x: 800,
+          y: 300,
+        },
+        {
+          name: 'Node 3',
+          x: 550,
+          y: 100,
+        },
+        {
+          name: 'Node 4',
+          x: 550,
+          y: 500,
+        },
+      ],
+      // links: [],
+      links: [
+        {
+          source: 0,
+          target: 1,
+          symbolSize: [5, 20],
+          label: {
+            show: true,
+          },
+          lineStyle: {
+            width: 5,
+            curveness: 0.2,
+          },
+        },
+        {
+          source: 'Node 2',
+          target: 'Node 1',
+          label: {
+            show: true,
+          },
+          lineStyle: {
+            curveness: 0.2,
+          },
+        },
+        {
+          source: 'Node 1',
+          target: 'Node 3',
+        },
+        {
+          source: 'Node 2',
+          target: 'Node 3',
+        },
+        {
+          source: 'Node 2',
+          target: 'Node 4',
+        },
+        {
+          source: 'Node 1',
+          target: 'Node 4',
+        },
+      ],
+      lineStyle: {
+        opacity: 0.9,
+        width: 2,
+        curveness: 0,
+      },
     },
   ],
 })
